c8d04cd85118bfb503857dbcf51ab6a0a10541be
mtpham017 <mxviiz17@gmail.com>
2023-09-30 17:14:27 -0500

    nathan stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,11 @@
 </svelte:head>
 
 <style>
+
+
+.container{
+  
+}
   /* Global Styles */
   :global(body) {
     font-family: 'Your-Font-Here', sans-serif; /* Replace with your desired font */
@@ -25,12 +30,14 @@
   .section-header {
     text-align: center;
     padding-bottom: 30px;
+    padding-top: 40px;
   }
 
-  .section-header h2 {
+  .about-section-bg{
     font-size: 1rem;
     font-weight: 400;
-    color: #7f7f90;
+    color: #000;
+    text-align: center;
     text-transform: uppercase;
   }
 
@@ -38,42 +45,29 @@
     font-size: 2rem;
     font-weight: 400;
     color: #000;
+    
+  }
+
+  .tech{
+    font-size: 25px;
+    font-weight: 400;
+    color: #000;
+    text-align: center;
+  }
+
+  .tech li{
+    list-style: none;
   }
 
   .section-header p span {
     color: #4c956c;
   }
 
-  /* Style the navigation bar */
-  nav {
-    background-color: #4c956c;
-    color: white;
-    padding: 1rem 0;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-
-  nav ul {
-    list-style: none;
-    display: flex;
-    gap: 1rem;
-  }
-
-  nav ul a {
-    text-decoration: none;
-    color: white;
-    transition: color 0.3s;
-  }
-
-  nav ul a:hover {
-    color: #fefee3;
-  }
 
   /* Style the "Get Started" button */
   .get-started-button {
     background-color: #2c6e49;
-    color: #fefee3;
+    color:#fefee3;
     border: none;
     padding: 10px 20px;
     border-radius: 5px;
@@ -81,43 +75,48 @@
     font-size: 1rem;
     margin-top: 1rem;
     transition: background-color 0.3s;
+    text-align:center;
   }
 
   .get-started-button:hover {
     background-color: #237d57;
   }
 
+  .get-started-section-bg{
+    font-size: 25px;
+    font-weight: 400;
+    color: #000;
+    text-align: center;
+    
+
+  }
+
   /* Style the footer */
   footer {
     margin-top: 2rem;
-    color: #777;
+    color: #000000;
     text-align: center;
   }
+
+
+
 </style>
 
 <div class="container">
-  <!-- Navigation bar -->
-  <nav>
-    <ul>
-      <li><a href="/">Home</a></li>
-      <li><a href="/">Fridge</a></li>
-    </ul>
-    <button class="get-started-button">Login</button>
-  </nav>
-
+ 
   <header class="section-header">
     <h1>Welcome to <span>Virtual Kitchen Helper</span></h1>
     <p>Your Virtual Kitchen Assistant for identifying ingredients, sharing nutritional facts, recommending recipes, and more.</p>
   </header>
 
-  <section class="about section-bg">
+  <section class="about-section-bg">
     <h2>About Virtual Kitchen Helper</h2>
     <p>
       Virtual Kitchen Helper is your all-in-one digital kitchen assistant. It helps you identify ingredients, shares nutritional facts, recommends delicious recipes based on what's in your pantry, and assists you in planning healthy and tasty meals.
     </p>
   </section>
 
-  <section class="technologies">
+  <section class="tech">
     <h2>Technologies We Use</h2>
     <ul>
       <li>SvelteKit - A powerful web framework</li>
@@ -128,7 +127,7 @@
     </ul>
   </section>
 
-  <section class="get-started section-bg">
+  <section class="get-started-section-bg">
     <h2>Get Started</h2>
     <p>
       Start your culinary journey with Virtual Kitchen Helper. Discover new recipes, plan your meals, and make cooking easier and more enjoyable.
@@ -139,5 +138,7 @@
   <footer>
     <p>&copy; 2023 Virtual Kitchen Helper. All rights reserved.</p>
   </footer>
+
+  
 </div>
 
