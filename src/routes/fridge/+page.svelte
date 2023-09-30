f9814225e1d00d36bef33a5b3f1d9f653aa54876
betsychen2002 <124523349+betsychen2002@users.noreply.github.com>
2023-09-30 11:21:20 -0500

    gridlayout fridge

--- src/routes/fridge/+page.svelte
+++ src/routes/fridge/+page.svelte
@@ -1,11 +1,34 @@
 <script lang="ts">
-import { Grid } from '@svelteuidev/core';
-import Fooddisplayer from '../../components/fooddisplayer.svelte';
-</script>
-
-
-<Grid>
-    <Grid.Col span={4}>1</Grid.Col>
-    <Grid.Col span={4}>2</Grid.Col>
-    <Grid.Col span={4}>3</Grid.Col>
-</Grid>
+    import { Grid } from '@svelteuidev/core';
+    import Fooddisplayer from '../../components/fooddisplayer.svelte';
+    import IngredientTextInput from '../../components/IngredientTextInput.svelte';
+  </script>
+  
+  <style>
+    /* Style the container to add padding */
+    .container {
+      padding: 20px; /* Adjust the padding as needed */
+    }
+  
+    /* Style the IngredientTextInput */
+    .input-container {
+      margin-bottom: 60px; /* Add space below the input */
+    }
+  </style>
+  
+  <div class="container">
+    <IngredientTextInput class="input-container "></IngredientTextInput>
+    <Grid>
+      <Grid.Col span={4}>
+        <Fooddisplayer />
+      </Grid.Col>
+      <Grid.Col span={4}>
+        <Fooddisplayer />
+      </Grid.Col>
+      <Grid.Col span={4}>
+        <Fooddisplayer />
+      </Grid.Col>
+      <!-- Add more Grid.Col elements as needed -->
+    </Grid>
+  </div>
+  
