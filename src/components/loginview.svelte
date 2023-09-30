1bd46d467bf5e7f308dd93537e7bf27927077d7b
betsychen2002 <124523349+betsychen2002@users.noreply.github.com>
2023-09-30 11:12:18 -0500

    tailwindcss

--- src/components/loginview.svelte
+++ src/components/loginview.svelte
@@ -3,6 +3,9 @@
     import { goto } from '$app/navigation';
     import type { ActionData } from './$types';
     import { PasswordInput, TextInput } from '@svelteuidev/core';
+    import {Button} from '@svelteuidev/core'
+    
+
   
     export let value = '';
     export let form: ActionData;
@@ -19,13 +22,76 @@
       isSuccess = true;
     }
   </script>
+ <style>
+    /* Style the container to center its contents */
+    .container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      min-height: 100vh;
+    }
   
-  <form on:submit="{handleSubmit}">
-    <PasswordInput bind:value />
-    <TextInput placeholder="User Name" label="Full name" />
+    /* Style the header */
+    .header {
+      font-size: 28px; /* Increase the font size for the header */
+      font-weight: bold;
+      margin-bottom: 20px;
+    }
   
-    <!-- Add a button to trigger form submission -->
-    <button type="submit">Submit</button>
-  </form>
+    /* Style the form */
+    .form {
+      width: 100%; /* Make the form full-width on mobile devices */
+      max-width: 400px;
+      padding: 20px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      text-align: center; /* Center text content within the form */
+    }
+  
+    /* Style the form elements */
+    .form-element {
+      margin-bottom: 16px;
+    }
+  
+    /* Style the Log In button */
+    .login-button {
+      display: inline-block; /* Ensure the button doesn't take up the full width */
+      align-items: center;
+
+    }
+  
+    /* Media query for mobile responsiveness */
+    @media (max-width: 768px) {
+      .header {
+        font-size: 24px; /* Reduce the font size for smaller screens */
+      }
+      .form {
+        padding: 10px; /* Decrease the padding for smaller screens */
+      }
+    }
+  </style>
+  
+  <div class="container">
+    <div class="header">Log In Here</div>
+    <div class="form">
+      <form on:submit="{handleSubmit}">
+        <div class="form-element">
+          <TextInput placeholder="User Name" label="" />
+        </div>
+        <div class="form-element">
+          <PasswordInput placeholder="Password" bind:value />
+        </div>
+        <div class="form-element">
+          <!-- Add a button to trigger form submission -->
+          <Button type="submit" class="login-button">Log in</Button>
+        </div>
+      </form>
+    </div>
+  </div>
+  
+  
+  
    
   
---
+++ src/layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { SvelteUIProvider } from '@svelteuidev/core';
+   
+
+</script>
+
+<SvelteUIProvider withNormalizeCSS withGlobalStyles>
+	<slot />
+</SvelteUIProvider>
