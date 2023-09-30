1e635921088b20232285d0e7b0909cd67bcaf86e
jacob <jacoobes@sern.dev>
2023-09-30 16:59:17 -0500

    fix auth

--- src/routes/(app)/fridge/+page.server.ts
+++ src/routes/(app)/fridge/+page.server.ts
@@ -1,6 +1,16 @@
+import { redirect, type Actions } from "@sveltejs/kit"
+import type { PageServerLoad } from "./$types"
+
+export const load = (async ({ parent, locals }) => {
+    const { user_id=null } = await parent().then(s => s.session)
+    if(!user_id) {
+        throw redirect(302, "/")
+    } 
+}) satisfies PageServerLoad
+
 export const actions = { 
 
     register: () => {
-        console.log('hell')
+        return {}
     }
-}
+} satisfies Actions
--- src/routes/(app)/fridge/recipe/+page.server.ts
+++ src/routes/(app)/fridge/recipe/+page.server.ts
@@ -1,5 +1,8 @@
+import type { PageServerLoad } from './$types'
 
+export const load = (async ({parent} ) => {
 
-export const load = async ({parent} ) => {
    console.log(await parent())
-}
+
+}) satisfies PageServerLoad
+
