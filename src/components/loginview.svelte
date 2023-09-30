ffe301db8b74a113213d55aab0b93b2c4d7d1d98
jacob <jacoobes@sern.dev>
2023-09-30 10:55:39 -0500

    hooks

--- src/components/loginview.svelte
+++ src/components/loginview.svelte
@@ -6,6 +6,10 @@
     export let form: ActionData
     export let isSuccess: boolean = false;
     
+    const redirectRegister = () => {
+        goto('/register') 
+    }
+    
     </script>
     
     <form method='POST' use:enhance>
@@ -19,9 +23,15 @@
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <form class="space-y-6" action="#" method="POST">
             <div>
+                <label for="business_id" class="block text-sm font-medium leading-6 text-gray-900">Business ID</label>
+                <div class="mt-2">
+                    <input id="business_id" name="business_id" type="number" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                </div>
+            </div>
+            <div>
                 <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
                 <div class="mt-2">
-                    <input id="email" name="email" type="email" autocomplete="email" required >
+                    <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                 </div>
             </div>
             <div>
@@ -40,4 +50,3 @@
             </div> -->
         </form>
     </div>
-
