f59afaa1c764e1cba9de128176fdc49b5b925fcd
betsychen2002 <124523349+betsychen2002@users.noreply.github.com>
2023-09-30 12:03:16 -0500

    recipe

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,9 +4,9 @@ import type { Handle } from '@sveltejs/kit'
 export const handle = (async ({ event, resolve }) => { 
 
     const access = event.cookies.get("access") === "true";
-    if(!access && event.route.id?.startsWith('/(app)')){ 
-        throw redirect(302, '/login')
-    }
+   // if(!access && event.route.id?.startsWith('/(app)')){ 
+   //     throw redirect(302, '/login')
+   // }
 
     return resolve(event);    
 }) satisfies Handle;
---
+++ src/routes/(app)/fridge/recipe/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { Grid } from '@svelteuidev/core';
+    import Fooddisplayer from '../../../components/fooddisplayer.svelte';
+    import IngredientTextInput from '../../../components/IngredientTextInput.svelte';
+    import GenerateRecipeButton from '../../../components/GenerateRecipeButton.svelte';
+  </script>
+  <IngredientTextInput></IngredientTextInput>
+  <GenerateRecipeButton></GenerateRecipeButton>
+  <Grid>
+    <Grid.Col span={4}>
+      <Fooddisplayer />
+    </Grid.Col>
+    <Grid.Col span={4}>
+      <Fooddisplayer />
+    </Grid.Col>
+    <Grid.Col span={4}>
+      <Fooddisplayer />
+    </Grid.Col>
+    <!-- Add more Grid.Col elements as needed -->
+  </Grid>
+  
