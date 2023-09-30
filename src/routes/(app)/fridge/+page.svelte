5e9a5c6724639a8c7f58af9e20acb60eeb52ba33
jacob <jacoobes@sern.dev>
2023-09-30 15:25:35 -0500

    fixing errors, form

--- src/routes/(app)/fridge/+page.svelte
+++ src/routes/(app)/fridge/+page.svelte
@@ -2,8 +2,11 @@
     import { Grid } from '@svelteuidev/core';
     import Fooddisplayer from '../../../components/fooddisplayer.svelte';
     import IngredientTextInput from '../../../components/IngredientTextInput.svelte';
+    import AddFoodButton from '../../../components/AddFoodButton.svelte';
   </script>
+
   <IngredientTextInput></IngredientTextInput>
+  <AddFoodButton/>
   <Grid>
     <Grid.Col span={4}>
       <Fooddisplayer />
