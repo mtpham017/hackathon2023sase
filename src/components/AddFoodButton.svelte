74efb6ee3717c3e79e6ce4b61738108975ef85c5
betsychen2002 <124523349+betsychen2002@users.noreply.github.com>
2023-09-30 13:44:35 -0500

    merge

---
+++ src/components/AddFoodButton.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+    import { Button } from '@svelteuidev/core';
+    import { TextInput } from '@svelteuidev/core';
+    import { insertItem } from '$lib/database';
+
+    let foodData = null;
+    let searchQuery = ''; // Initialize search query
+
+  // Function to fetch food data from the Open Food Repo API
+  // Use the API key stored in the environment variable
+  const apiKey = process.env.OPENFOODREPO_API_KEY;
+
+  async function fetchFoodData() {
+  try {
+    const apiUrl = 'https://api.openfoodrepo.org/v1/products';
+
+    // Include the search query in the API URL
+    const apiUrlWithQuery = `${apiUrl}?query=${searchQuery}`;
+
+    // Create headers and include the API key
+    const headers = new Headers({
+      'Authorization': `Bearer ${apiKey}`,
+      'Content-Type': 'application/json', // Include content type if required
+    });
+
+    const response = await fetch(apiUrlWithQuery, {
+      method: 'GET',
+      headers: headers,
+    });
+
+    if (response.ok) {
+      const apiData = await response.json();
+
+      // Iterate through the API data and insert each item into the database
+      apiData.forEach((apiItem: { name: any; barcode: any; expiration_date: any; category_id: any; user_id: any; image: WithImplicitCoercion<string> | { [Symbol.toPrimitive](hint: "string"): string; }; }) => {
+        // Convert API data to the format expected by the database
+        const insertItem = {
+          name: apiItem.name,
+          barcode: apiItem.barcode,
+          expirationDate: apiItem.expiration_date,
+          categoryId: apiItem.category_id,
+          userId: apiItem.user_id,
+          image: Buffer.from(apiItem.image, 'base64'), // Assuming the API returns image data as base64
+        };
+
+        // Insert the item into the database
+        insertItem(apiItem);
+      });
+    } else {
+      console.error('Failed to fetch food data');
+    }
+  } catch (error) {
+    console.error('An error occurred while fetching food data:', error);
+  }
+}
+
+  </script>
+<TextInput
+input type="text" bind:value={searchQuery} placeholder="Enter ingredients" 
+ />
+  <Button on:click={fetchFoodData}>
+    Add to Fridge
+  </Button>
