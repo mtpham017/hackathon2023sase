1c34af0f4e56b0371f1f225046b449ed9c975ceb
betsychen2002 <124523349+betsychen2002@users.noreply.github.com>
2023-10-01 02:37:45 -0500

    api endpoints

--- src/routes/api/item/+server.ts
+++ src/routes/api/item/+server.ts
@@ -1,8 +1,14 @@
 import { json, type RequestHandler } from "@sveltejs/kit";
-import { insertItem } from '$lib/database'
+import { getItemsByUserId, insertItem } from '$lib/database'
 
 export const POST = ( async ({ request }) => {
     const body = await request.json();
     return json(insertItem(body)) 
 
 }) satisfies RequestHandler
+
+export const GET: RequestHandler = async ({ request}) => {
+    const userId =  await request.json(); // Extract user_id from the request parameters
+    const items = getItemsByUserId(userId);
+    return json(items);
+};
