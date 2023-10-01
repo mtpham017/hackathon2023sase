4f0a71cca0b1d9f8107075a06b30f1486ef84e74
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2023-09-30 20:33:53 -0500

    recipe

--- src/routes/(app)/fridge/recipe/+page.server.ts
+++ src/routes/(app)/fridge/recipe/+page.server.ts
@@ -1,8 +1,18 @@
+import { getItemsByUserId, getRecipeById, getRecipesByUserId } from '$lib/database'
 import type { PageServerLoad } from './$types'
 
 export const load = (async ({parent} ) => {
+   const { user_id, email } = (await parent()).session
 
-   console.log(await parent())
+   const food = getItemsByUserId(user_id)
 
+   const recipes = getRecipesByUserId(user_id)
+   console.log(food)
+   console.log(recipes)
+   return {
+      food,
+      user_id,
+      email
+   }
 }) satisfies PageServerLoad
 
