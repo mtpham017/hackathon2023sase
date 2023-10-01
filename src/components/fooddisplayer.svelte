01733476cfdfcfb606776013395462256b5265e5
betsychen2002 <124523349+betsychen2002@users.noreply.github.com>
2023-09-30 21:33:24 -0500

    fooddata

--- src/components/fooddisplayer.svelte
+++ src/components/fooddisplayer.svelte
@@ -1,73 +1,46 @@
-<script>
-	import { Badge, Button, Card, Group, Image, Text } from '@svelteuidev/core';
-	import {onMount} from 'svelte';
- 
-
-	/**
-     * @type {{ image_url: any; product_name: any; description: any; protein: any; carbohydrates: any; fat: any; }}
-     */
-	let foodData;
- 
-	// Function to fetch food data from the Open Food Repo API
-	async function fetchFoodData() {
-		try {
-			const response = await fetch('https://api.openfoodrepo.org/v1/products');  
-			if (response.ok) {
-				foodData = await response.json();
-			} else {
-				console.error('Failed to fetch food data');
-			}
-		} catch (error) {
-			console.error('An error occurred while fetching food data:', error);
-		}
-	}
-
-	// Call the fetchFoodData function when the component is mounted
-	onMount(() => {
-		fetchFoodData();
-	});
+<script lang='ts'>
+import { applyAction } from '$app/forms';
+import { Badge, Button, Card, Group, Image, Text } from '@svelteuidev/core';
+export let food : App.FoodData
 </script>
 
-{#if foodData}
-	<Card shadow='sm' padding='lg'>
-		<Card.Section first padding='lg'>
-			<Image
-				src={foodData.image_url}  
-				height={160}
-				alt={foodData.product_name}
-			/>
-		</Card.Section>
 
-		<Group position='apart'>
-			<Text weight={500}>{foodData.product_name}</Text>
-			<Badge color='pink' variant='light'>
-				On Sale
-			</Badge>
-		</Group>
+<Card shadow="sm" padding="lg">
+  <Card.Section first padding="lg">
+    <Image src={Food} height={160} alt={name} />
+  </Card.Section>
 
-		<Text size='sm'>
-			{foodData.description}
-		</Text>
+  <Group position="apart">
+    <Text weight={500}>{name}</Text>
+  </Group>
 
-		<Text size='sm'>
-			Nutritional Information:
-		</Text>
+  <Text size="sm">
+    {description}
+  </Text>
 
-		<!-- Render nutritional information, replace with actual keys from your data -->
-		<Text size='sm'>
-			Protein: {foodData.protein}g
-		</Text>
-		<Text size='sm'>
-			Carbohydrates: {foodData.carbohydrates}g
-		</Text>
-		<Text size='sm'>
-			Fat: {foodData.fat}g
-		</Text>
+  <Text size="sm">
+    Nutritional Information:
+  </Text>
 
-		<Button variant='light' color='blue' fullSize>
-			Book classic tour now
-		</Button>
-	</Card>
-{:else}
-	<p>Loading food data...</p>
-{/if}
+  <Text size="sm">
+    Protein: {nutrient.protein}
+  </Text>
+  <Text size="sm">
+    Carbohydrates: {nutrient.carbs}
+  </Text>
+  <Text size="sm">
+    Sodium: {nutrient.sodium}
+  </Text>
+  <Text size="sm">
+    Fiber: {nutrient.fiber}
+  </Text>
+  <Text size="sm">
+    Calories: {nutrient.calories}
+  </Text>
+
+  <Button variant="light" color="blue" fullSize>
+    Book classic tour now
+  </Button>
+</Card>
+
+ 
--- src/routes/(app)/fridge/+page.server.ts
+++ src/routes/(app)/fridge/+page.server.ts
@@ -1,6 +1,91 @@
-export const actions = { 
+import { insertItem } from '$lib/database.js';
+import type { Action } from './$types'
+import { env } from '$env/dynamic/private'
+import { setContext } from 'svelte';
+import type Fooddisplayer from '../../../components/fooddisplayer.svelte';
 
-    register: () => {
-        console.log('hell')
+interface FoodData {
+    name: string 
+    barcode : string 
+    score :number 
+    nutrients : number 
+    categoryID : number
+    userID : number 
+    image: string 
+
+}
+export const load  = async() => {
+    let searchQuery = ''
+    const apiKey = env.OPENFOODREPO_API_KEY;
+    let food: FoodData[] = [];
+    try {
+    const apiUrl = 'https://www.foodrepo.org/api/v3/products/_search'+"?api_key="+apiKey;
+   // Include the search query in the API URL
+    // Create headers and include the API key
+   const headers = new Headers({
+     'Authorization': `Token token=${apiKey}`,
+     'Content-Type': 'application/vnd.api+json', // Include content type if required
+     'Accept': 'application/json'
+   });
+
+   const requestBody ={
+    _source: {
+        includes: ["barcode", "name", "images","nutrients", "name_translations.en"]           
+    },
+    size:20,
+    query: {
+        query_string: {
+            query: searchQuery
+        }
+      }
+   }
+
+   const response = await fetch(apiUrl, {
+     method: 'POST',
+     headers: headers,
+     body: JSON.stringify(requestBody),
+   });
+
+
+   if (response.ok) {
+
+    const apiData = await response.json();
+     console.log(apiData.hits.hits)
+    // Iterate through the API data and insert each item into the database
+    const mapToFoodData = apiData.hits.hits.map((element: any) => {
+        const { carbohydrates, sodium, fiber, protein, energy_calories_kcal } = element._source.nutrients
+        console.log(element._source.nutrients)
+        return {
+            name: element?.name_translation?.en,
+            score: element._score,
+            barcode: element._source.barcode,
+            nutrient: {
+                carbs: carbohydrates?.per_hundred ? carbohydrates?.per_hundred + carbohydrates.unit : "?", 
+                sodium: sodium?.per_hundred ? sodium.per_hundred + sodium.unit : "?", 
+                fiber: fiber?.per_hundred ? fiber?.per_hundred + fiber.unit : "?",
+                protein: protein?.per_hundred ? protein?.per_hundred + protein.unit : "?",
+                calories: energy_calories_kcal?.per_hundred ? energy_calories_kcal?.per_hundred + energy_calories_kcal.unit : "?"
+    
+            }  ,
+            categoryId: null,
+            userId: 1,
+            image:Buffer.from(element._source.images?.[0]?.medium ?? "", 'base64'),
+        };
+    })
+
+    food = mapToFoodData(apiData);
+    return {
+    food
     }
+   } else {
+    return{
+        food: []
+    }
+  }
+} catch (error) {
+  console.error('An error occurred while fetching food data:', error)
+  return {
+    food: []
+  };
+}
 }
