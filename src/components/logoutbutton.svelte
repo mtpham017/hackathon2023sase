75da77a41069513ccc3c9706f11c1c4bcc8ab660
jacob <jacoobes@sern.dev>
2023-09-30 19:47:04 -0500

    fix logout

--- src/components/logoutbutton.svelte
+++ src/components/logoutbutton.svelte
@@ -1,31 +1,8 @@
 <script lang="ts">
     import { goto } from '$app/navigation';
     import {Button} from '@svelteuidev/core'
-  
-    const handleLogout = async () => {
-      try {
-        const response = await fetch('/logout', {
-          method: 'DELETE',
-          headers: {
-            'Content-Type': 'application/json',
-            // You might need to include authentication headers here
-          },
-          body: JSON.stringify({ token: REFRESH_TOKEN_SECRET }), // Replace with actual refresh token
-        });
-  
-        if (response.ok) {
-           goto('/');
-        } else {
-           console.error('Logout failed');
-        }
-      } catch (error) {
-        console.error('Error during logout:', error);
-      }
-    };
   </script>
   
-  <Button
-    on:click={handleLogout}
-  >
+  <Button on:click={() => { goto("/") }}>
     Logout
   </Button>
