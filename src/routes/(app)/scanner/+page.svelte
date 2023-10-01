8d2323417afe3ec0f61f22d232ba4981918f78dc
jacob <jacoobes@sern.dev>
2023-09-30 19:42:43 -0500

    barcode

--- src/routes/(app)/scanner/+page.svelte
+++ src/routes/(app)/scanner/+page.svelte
@@ -2,6 +2,36 @@
     import BarcodeScanner from "../../../components/barcodeScanner.svelte";
 </script>
 
-<BarcodeScanner>
-    
-</BarcodeScanner>
+<div id="the-div"> 
+    <BarcodeScanner/>
+</div>
+
+
+
+<style>
+	div {
+		display: flex;
+		/* vertical */
+		align-items: center;
+		/* horizontal */
+		justify-content: center;
+	}
+
+  #the-div {
+		flex-grow: 1;
+	}
+
+  pre {
+    background: white;
+		padding: 2em;
+		box-shadow: 100px;
+	}
+
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		align-items: stretch;
+		flex: 1;
+	}
+</style>
